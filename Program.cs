d0b31cc6d3f4aba8bdcb9588715712750ce1b7db
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-25 09:21:26 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace intasinputd6
+{
+    internal class program
+    {
+        public static void main(String[] args)
+        {
+            Console.WriteLine("converting binary using binary function");
+        }
+    }
+}
